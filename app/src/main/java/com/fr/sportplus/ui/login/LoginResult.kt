7d017b6becad2e477609ce2a5fc9943208056ced
DoFaure <dorianfaure69@gmail.com>
2021-12-13 22:46:16 +0100

    init project

---
+++ app/src/main/java/com/fr/sportplus/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.fr.sportplus.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
